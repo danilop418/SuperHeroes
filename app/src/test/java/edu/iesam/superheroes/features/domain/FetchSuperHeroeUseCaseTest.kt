89faa714986fd8d11beef3a0022394c93f7e656a
LGD <daniel.lopgon.4@educa.jcyl.es>
2025-10-02 15:47:19 +0200

    Commit : I added unit test

--- app/src/test/java/edu/iesam/superheroes/features/domain/FetchSuperHeroeUseCaseTest.kt
+++ app/src/test/java/edu/iesam/superheroes/features/domain/FetchSuperHeroeUseCaseTest.kt
@@ -1,7 +1,62 @@
 package edu.iesam.superheroes.features.domain
 
+import io.mockk.every
+import io.mockk.mockk
 import org.junit.Assert.*
+import org.junit.Test
 
 class FetchSuperHeroeUseCaseTest {
+
+    @Test
+    fun `when repository return succes then usecase returns succes`() {
+        //Given
+        val superheroeRepositoryMockk = mockk<SuperHeroeRepository>()
+        val expectedHeroes = listOf(
+            SuperHeroe("1", "Solaris", "solaris", "https://example.com/images/solaris.jpg"),
+            SuperHeroe(
+                "2",
+                "Aquamancer",
+                "aquamancer",
+                "https://example.com/images/aquamancer.jpg"
+            ),
+            SuperHeroe(
+                "3",
+                "Voltstrike",
+                "voltstrike",
+                "https://example.com/images/voltstrike.jpg"
+            ),
+            SuperHeroe(
+                "4",
+                "Shadowlynx",
+                "shadowlynx",
+                "https://example.com/images/shadowlynx.jpg"
+            ),
+        )
+
+        every { superheroeRepositoryMockk.fetch() } returns Result.success(expectedHeroes)
+
+        val superHeroeUseCase = FetchSuperHeroeUseCase(superheroeRepositoryMockk)
+
+        // When
+        val result = superHeroeUseCase.fetch()
+
+        //Then
+        assertTrue(result.isSuccess)
+        assertEquals(expectedHeroes, result.getOrNull())
+    }
+
+    @Test
+    fun `when repository returns internet error then usecase returns failure`() {
+        // Given
+        val superheroeRepositoryMockk = mockk<SuperHeroeRepository>()
+        every { superheroeRepositoryMockk.fetch() } returns Result.failure(ErrorApp.InternetConexionError)
+
+        val superHeroeUseCase = FetchSuperHeroeUseCase(superheroeRepositoryMockk)
+        // When
+        val result = superHeroeUseCase.fetch()
+
+        // Then
+        assertTrue(result.isFailure)
+    }
 
 }
