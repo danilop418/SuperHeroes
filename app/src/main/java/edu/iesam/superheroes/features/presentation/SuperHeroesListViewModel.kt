8c786d016ccbc7eafabad27828822683e43bec8a
danilop418 <daniel.lopgon.4@educa.jcyl.es>
2025-10-04 10:05:28 +0200

    Commit : I added into viewModel the handling error

--- app/src/main/java/edu/iesam/superheroes/features/presentation/SuperHeroesListViewModel.kt
+++ app/src/main/java/edu/iesam/superheroes/features/presentation/SuperHeroesListViewModel.kt
@@ -1,14 +1,25 @@
 package edu.iesam.superheroes.features.presentation
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import edu.iesam.superheroes.features.domain.ErrorApp
 import edu.iesam.superheroes.features.domain.FetchSuperHeroeUseCase
 import edu.iesam.superheroes.features.domain.SuperHeroe
 
-class SuperHeroesListViewModel(private val fetchSuperHeroeUseCase: FetchSuperHeroeUseCase):
+class SuperHeroesListViewModel(private val fetchSuperHeroeUseCase: FetchSuperHeroeUseCase) :
     ViewModel() {
+    private val _superheroes = MutableLiveData<List<SuperHeroe>>()
+    val superheroes: LiveData<List<SuperHeroe>> = _superheroes
 
-    fun fetch(): Result<List<SuperHeroe>>{
-        return fetchSuperHeroeUseCase.fetch()
+    private val _error = MutableLiveData<ErrorApp>()
+    val error: LiveData<ErrorApp> = _error
+
+    fun loadSuperheroes() {
+        val result = fetchSuperHeroeUseCase.fetch()
+        result.fold(
+            { heroes -> _superheroes.value = heroes },
+            { error -> _error.value = error as ErrorApp }
+        )
     }
-
 }
