106906fcc860ceb4e80f6760b1829dfb150851ce
danilop418 <daniel.lopgon.4@educa.jcyl.es>
2025-10-05 19:24:19 +0200

    Commit: I added glide

--- app/src/main/java/edu/iesam/superheroes/features/presentation/SuperHeroesAdapter.kt
+++ app/src/main/java/edu/iesam/superheroes/features/presentation/SuperHeroesAdapter.kt
@@ -6,6 +6,7 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.superheroes.R
 import edu.iesam.superheroes.features.domain.SuperHeroe
 
@@ -40,6 +41,16 @@ class SuperHeroesAdapter(
         fun bind(superhero: SuperHeroe) {
             tvName.text = superhero.name
             tvRealName.text = superhero.slug ?: "Identidad desconocida"
+            itemView.setOnClickListener {
+                onItemClick(superhero)
+            }
+            Glide.with(itemView.context)
+                .load(superhero.urlImage)
+                .placeholder(R.drawable.ic_launcher_foreground)
+                .error(R.drawable.ic_launcher_foreground)
+                .centerCrop()
+                .into(ivSuperhero)
+
             itemView.setOnClickListener {
                 onItemClick(superhero)
             }
